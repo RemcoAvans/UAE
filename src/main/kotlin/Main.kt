6af5a330ce22182ab9a0106a41cd44592a87660f
Dpragt <dpragt@hotmail.nl>
2025-10-02 16:10:16 +0200

    Task list html result via api

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import io.ktor.http.ContentType
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.application.*
@@ -10,11 +11,23 @@ fun main() {
             get("/") {
                 call.respondText("Hello World!")
             }
+            get("/todo") {
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = """
+                <h5>Hier kunnen we taken plaatsen die gedaan moeten worden</h5>
+                <h3>TODO list:</h3>
+                <ol>
+                    <li>Request controllers maken</li>
+                    <li>Use case base-class/interface maken</li>
+                    <li>Repository interface maken</li>
+                    <li>Repository implementatie maken voor of inMemory-db of Sql-server</li>
+                </ol>
+                """.trimIndent()
+                )
+            }
             get("/check") {
                 call.respondText("Check OK!")
-            }
-            get("/JaronIs") {
-                call.respondText("Hoi!")
             }
             get("/remco") {
                 call.respondText("Hallo!")
