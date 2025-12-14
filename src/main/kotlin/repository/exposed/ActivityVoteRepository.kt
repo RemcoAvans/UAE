7aa8d2c304d9b91646c9ffeb4b61ba3814dc6734
Dpragt <dpragt@hotmail.nl>
2025-12-14 12:42:46 +0100

    Table objects met AvtivityTagRepository

--- src/main/kotlin/repository/exposed/ActivityVoteRepository.kt
+++ src/main/kotlin/repository/exposed/ActivityVoteRepository.kt
@@ -1,34 +1,61 @@
 package repository.exposed
 
+import com.example.data.models.ActivityTable
+import com.example.data.models.ActivityTagTable
+import com.example.data.models.ActivityVoteTable
+import com.example.model.ActivityTag
 import com.example.model.ActivityVote
 import com.example.repository.IActivityVoteRepository
+import com.example.repository.exposed.DatabaseHelper
 import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import model.Activity
+import org.h2.api.H2Type.row
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.selectAll
+import kotlin.Int
 
 open class ActivityVoteRepository : IActivityVoteRepository {
 
+    private fun toActivityVote(row: ResultRow): ActivityVote =
+        ActivityVote(
+            id = row[ActivityVoteTable.id].value,
+            activityId = row[ActivityVoteTable.activityId],
+            userId = row[ActivityVoteTable.userId],
+            createAt = LocalDate.parse(row[ActivityVoteTable.createAt]),
+            positive = row[ActivityVoteTable.positive],
+        )
+
+    //To delete
     private val votes = mutableListOf<ActivityVote>()
 
-    override suspend fun getAll(): List<ActivityVote> = votes
+    override suspend fun getAll(): List<ActivityVote> = DatabaseHelper.dbQuery {
+        ActivityVoteTable
+            .selectAll()
+            .map(::toActivityVote)
+    }
 
-    override suspend fun getById(id: Int): ActivityVote? = votes.find { it.id == id }
+    override suspend fun getById(id: Int): ActivityVote? = DatabaseHelper.dbQuery  {
+        ActivityVoteTable
+            .selectAll().where { ActivityVoteTable.id eq id }.toList()
+            .map(::toActivityVote)
+            .singleOrNull()
+    }
 
-    open override  suspend fun getByQuery(predicate: (ActivityVote) -> Boolean): List<ActivityVote> =
-        votes.filter(predicate)
+    open override  suspend fun getByQuery(predicate: (ActivityVote) -> Boolean): List<ActivityVote>  =
+        getAll().filter(predicate)
 
-    override suspend fun create(entity: ActivityVote): ActivityVote {
-        val nextId = (votes.maxOfOrNull { it.id } ?: 0) + 1
-        val today: LocalDate = Clock.System.now()
-            .toLocalDateTime(TimeZone.currentSystemDefault())
-            .date
-        val newVote = entity.copy(
-            id = nextId,
-            createAt = today
-        )
-        votes.add(newVote)
-        return newVote
+    override suspend fun create(entity: ActivityVote): ActivityVote = DatabaseHelper.dbQuery {
+        val id = ActivityVoteTable.insertAndGetId { row ->
+            row[activityId] = entity.activityId
+            row[userId] = entity.userId
+            row[createAt] = entity.createAt.toString()
+            row[positive] = entity.positive
+        }
+        entity.copy(id = id.value)
     }
 
     override suspend fun update(entity: ActivityVote): Boolean {
