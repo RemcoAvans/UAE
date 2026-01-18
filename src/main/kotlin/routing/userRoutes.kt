ba637d7647cb91428a8c17dfed842708b323ce85
Dpragt <dpragt@hotmail.nl>
2026-01-18 12:42:30 +0100

    fix is Admin

--- src/main/kotlin/routing/userRoutes.kt
+++ src/main/kotlin/routing/userRoutes.kt
@@ -122,14 +122,16 @@ fun Route.userRoutes(repo: IUserRepository) {
             get("isAdmin"){
                 val principal = call.principal<JWTPrincipal>()
                 if (principal == null) {
-                    call.unauthorized("Unauthenticated user")
+                    call.ok(false)
                     return@get
                 }
                 val role = principal.payload.getClaim("role").asString()
                 if (role == "Admin"){
                     call.ok(true)
+                    return@get
                 } else {
                     call.ok(false)
+                    return@get
                 }
             }
 
